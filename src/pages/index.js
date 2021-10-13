bf3da7f3106e2cfa24265edfd1e0f2722f146ca1
Naga Chaitanya Konada <knc.dev@outlook.com>
2021-10-13 16:40:17 +0530

    feat: bootstrap and styles.scss added

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,13 @@ import * as React from "react"
 // markup
 const IndexPage = () => {
   return (
-    <main>
-      <h1>Hello World</h1>
+    <main className="container">
+      <header className="row">
+        <nav className="col">
+          <h1>Hello World</h1>
+          <button className="btn btn-primary rounded rounded-pill">Click me</button>
+        </nav>
+      </header>
     </main>
   )
 }

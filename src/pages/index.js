391d93e3af974b5d066185ce760d6986c00167bb
Naga Chaitanya Konada <knc.dev@outlook.com>
2021-10-13 21:00:31 +0530

    feat: Playing with bootstrap - primary & secondary colors changed - some variables overridden - card component created

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,29 @@
+import { Link } from "gatsby"
 import * as React from "react"
+import Card from "../components/Card"
 
 // markup
 const IndexPage = () => {
   return (
-    <main className="container">
-      <header className="row">
-        <nav className="col">
-          <h1>Hello World</h1>
-          <button className="btn btn-primary rounded rounded-pill">Click me</button>
+    <main className="container-fluid my-2">
+      <header className="mb-5">
+        <nav className="col d-flex align-items-baseline justify-content-between">
+          <h1 className="fs-5 d-inline-block">
+            <Link to="/" className="text-decoration-none">Naga Konada</Link>
+          </h1>
+          <button className="btn btn-outline-primary btn-sm">Portfolio</button>
         </nav>
       </header>
+      <section className="row justify-content-center px-2 px-sm-0">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </section>
     </main>
   )
 }

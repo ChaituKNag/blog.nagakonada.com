4559f465dd410c8e1adb355e22cba1e75080683d
Naga Chaitanya Konada <knc.dev@outlook.com>
2021-10-13 23:21:34 +0530

    refactor: card content change

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,11 +4,10 @@ const Card = () => {
     return (
         <div className="card m-2 col-12 col-sm-5 col-lg-3 shadow-sm">
           <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <h5 className="card-title">Post title</h5>
+            <h6 className="card-subtitle mb-2 text-muted fs-6">Date: 20th August 2019</h6>
             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" className="card-link text-decoration-none">Card link</a>
-            <a href="#" className="card-link text-decoration-none">Another link</a>
+            <a href="#" className="card-link text-decoration-none">Read</a>
           </div>
         </div>
     )

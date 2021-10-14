bb42082e7713e75ce64c75224f0e4f73e868da70
Naga Chaitanya Konada <knc.dev@outlook.com>
2021-10-14 14:34:19 +0530

    feat: Header component created

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "gatsby";
+
+const Header = () => {
+  return (
+    <header className="mb-5 mt-3">
+      <nav className="col d-flex align-items-baseline justify-content-between position-relative">
+        <h1 className="fs-5 d-inline-block flex-grow-1 text-start text-md-center">
+          <Link to="/" className="text-decoration-none">
+            Naga Konada
+          </Link>
+        </h1>
+        <div className="position-absolute end-0 me-2">
+          <a
+            href="https://tech.nagakonada.com"
+            className="btn btn-outline-secondary btn-sm me-1"
+          >
+            Tech
+          </a>
+          <a
+            href="https://nagakonada.com"
+            className="btn btn-outline-primary btn-sm"
+          >
+            Portfolio
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;

1f1354c406c3f3aa16458984ae8fcef437d6d528
Naga Chaitanya Konada <knc.dev@outlook.com>
2021-10-15 00:02:46 +0530

    Card content coming from data

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,22 @@
-import { Link } from 'gatsby'
-import React from 'react'
+import { Link } from "gatsby";
+import React from "react";
 
-const Card = () => {
-    return (
-        <div className="card m-3 col-12 col-sm-5 col-lg-3 shadow-sm">
-          <div className="card-body">
-            <h5 className="card-title">Post title</h5>
-            {/* <h6 className="card-subtitle mb-2 text-muted fs-6">Date: 20th August 2019</h6> */}
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <Link to="/" className="card-link btn btn-sm btn-outline-primary text-decoration-none">Read</Link>
-          </div>
-        </div>
-    )
-}
+const Card = ({ post: { postId, title, description } }) => {
+  return (
+    <div className="card m-3 col-12 col-sm-5 col-lg-3 shadow-sm">
+      <div className="card-body">
+        <h5 className="card-title">{title}</h5>
+        {/* <h6 className="card-subtitle mb-2 text-muted fs-6">Date: 20th August 2019</h6> */}
+        <p className="card-text">{description}</p>
+        <Link
+          to={`/post/${postId}`}
+          className="card-link btn btn-sm btn-outline-primary text-decoration-none"
+        >
+          Read
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,49 @@
-import * as React from "react";
+import React, { useState } from "react";
 import Card from "../components/Card";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 
+const posts = [
+  {
+    title: "Lorem, ipsum dolor.",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur cumque eos voluptatibus.",
+    postId: 1
+  },
+  {
+    title: "Repellat, quos quisquam!",
+    description:
+      "Atque facere aliquam fugiat accusantium illum magni beatae hic sunt repudiandae et!",
+    postId: 2
+  },
+  {
+    title: "Omnis, quisquam similique.",
+    description:
+      "Aliquid modi excepturi quasi adipisci! Labore error laborum voluptates perferendis distinctio eum?",
+    postId: 3
+  },
+  {
+    title: "Maiores, illo? Quibusdam.",
+    description:
+      "Ipsa hic quidem veniam sapiente? Id consequuntur laborum eos delectus ad dignissimos!",
+    postId: 4
+  },
+  {
+    title: "Assumenda, sunt officia!",
+    description:
+      "Accusantium fuga totam sunt inventore nisi, minima sed? Eos officiis quis ratione?",
+    postId: 5
+  }
+];
+
 const IndexPage = () => {
+  const [searchText, setSearchText] = useState("");
+  const handleSearchChange = (e) => setSearchText(e.target.value);
+  const filtered = posts.filter(
+    (post) =>
+      post.title.toLowerCase().includes(searchText.toLowerCase()) ||
+      post.description.toLowerCase().includes(searchText.toLowerCase())
+  );
   return (
     <div className="container-fluid my-2">
       <Header />
@@ -11,25 +51,24 @@ const IndexPage = () => {
         <Hero />
         <div className="row justify-content-center">
           <div className="col-12 col-md-6">
-            <div class="input-group mb-3">
+            <div className="input-group mb-3">
               <input
                 type="search"
-                class="form-control rounded rounded-pill"
+                className="form-control rounded rounded-pill"
                 placeholder="Search topic"
                 aria-label="Search topic"
+                defaultValue={searchText}
+                onChange={handleSearchChange}
               />
             </div>
           </div>
         </div>
         <section className="row justify-content-center px-2 px-sm-0">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {filtered.length > 0 ? (
+            filtered.map((post) => <Card key={post.postId} post={post} />)
+          ) : (
+            <p className="text-center fs-5">Nothing to read, stay tuned!</p>
+          )}
         </section>
       </main>
     </div>

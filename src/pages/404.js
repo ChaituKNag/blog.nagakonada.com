97ab828c504301c0ec5dbf6a0a72348641e8d3d5
Naga Chaitanya Konada <knc.dev@outlook.com>
2021-10-14 14:34:28 +0530

    feat: 404 page formatted

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,52 +1,13 @@
 import * as React from "react"
 import { Link } from "gatsby"
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
 // markup
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
+    <main className="m-5 d-flex flex-column align-items-center">
+      <h1 className="fs-3">Oops, nothing here.</h1>
+      <p>May be try home page?</p>
+      <Link to="/" className="btn btn-lg btn-outline-primary">Yes, take me there</Link>
     </main>
   )
 }

1084518769b9b199e738ef54f8f7aa909c51df3c
Naga Chaitanya Konada <knc.dev@outlook.com>
2021-10-14 14:34:09 +0530

    feat: Card styles adjusted

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,14 @@
+import { Link } from 'gatsby'
 import React from 'react'
 
 const Card = () => {
     return (
-        <div className="card m-2 col-12 col-sm-5 col-lg-3 shadow-sm">
+        <div className="card m-3 col-12 col-sm-5 col-lg-3 shadow-sm">
           <div className="card-body">
             <h5 className="card-title">Post title</h5>
-            <h6 className="card-subtitle mb-2 text-muted fs-6">Date: 20th August 2019</h6>
+            {/* <h6 className="card-subtitle mb-2 text-muted fs-6">Date: 20th August 2019</h6> */}
             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" className="card-link text-decoration-none">Read</a>
+            <Link to="/" className="card-link btn btn-sm btn-outline-primary text-decoration-none">Read</Link>
           </div>
         </div>
     )

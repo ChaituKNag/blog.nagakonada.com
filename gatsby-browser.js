cb42604cadd2ca7d90510e14436bec3c6dc6c74b
Naga Chaitanya Konada <knc.dev@outlook.com>
2021-10-14 14:33:44 +0530

    feat: Fira Sans font added and scss refactoring

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,4 @@
-import '@fontsource/fira-sans-condensed'
-import './src/styles.scss';
+import '@fontsource/fira-sans/400.css'
+import '@fontsource/fira-sans/300.css'
+import '@fontsource/fira-sans/700.css'
+import './src/scss/styles.scss';
